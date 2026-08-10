3dad411c65be18c5bd25377a13dbbdb32b901592
home <home@DESKTOP-J7UD23O>
2026-08-10 16:29:03 -0400

    login y register casi terminados

---
+++ lib/utils/routes.dart
@@ -0,0 +1,8 @@
+class AppRoutes {
+  static const splash = '/';
+  static const login = '/login';
+  static const register = '/register';
+  static const recoverPassword = '/recover-password';
+  static const completeProfile = '/complete-profile';
+  static const home = '/home';
+}
